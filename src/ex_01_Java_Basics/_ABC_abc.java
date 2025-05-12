cfd646b355834948bd464b6181d9ba92d234a68c
Krishna <knakum39@gmail.com>
2025-05-12 22:43:28 +0530

    11th May 2025

--- src/ex_01_Java_Basics/_ABC_abc.java
+++ src/ex_01_Java_Basics/_ABC_abc.java
@@ -1,21 +1,4 @@
 package ex_01_Java_Basics;
 
 public class _ABC_abc {
-
-    public static void main(String[] args) {
-        System.out.println("Test Class Name : _ABC_abc");
-    }
-
 }
-
-/*
-package ex_01_Java_Basics;
-
-//java: underscore not allowed here
-//_A: it will be allowed.
-public class _ {
-    public static void main(String[] args) {
-        System.out.println("Test Class Name : _");
-    }
-
-}*/
--- src/ex_01_Java_Basics/_Prafull.java
+++
@@ -1,8 +0,0 @@
-package ex_01_Java_Basics;
-
-public class _Prafull {
-
-    public static void main(String[] args) {
-        System.out.println("Test Class Name : _Prafull");
-    }
-}
