6c218f1a0811b08b82b58709108fc2f2ce3762a6
Krishna <knakum39@gmail.com>
2025-05-12 22:45:15 +0530

    11th May 2025

--- src/ex_02_Java_Basics_Part2/Lab020_Interview_QnA2.java
+++ src/ex_02_Java_Basics_Part2/Lab020_Interview_QnA2.java
@@ -5,7 +5,7 @@ public class Lab020_Interview_QnA2 {
         // int enum = 10;
         int Enum = 10;
         int a_b = 89;
-//        int pramod dutta  = 89;
+//        int pramod dutta  = 87;
         int pramod$dutta = 90;
         int Lab020_Interview_QnA2 = 123;
 
