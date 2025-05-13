83e8ffec457e47576ac3156e73f9257362b889c4
Krishna <knakum39@gmail.com>
2025-05-13 18:00:44 +0530

    13th May 2025

---
+++ src/ex_02_Java_Basics_Part2/Lab022_Numerics_Data_Types.java
@@ -0,0 +1,51 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab022_Numerics_Data_Types {
+    public static void main(String[] args) {
+
+        //byte b = 128; // -128 to 127
+        byte b = 10;
+        int i = 10;
+        short s = 10;
+        char c  = 'A';
+        long l = 9876543210l;
+        long l1 = 9876543210L;
+
+        float f = 3.14f;
+        float f1 = 3.14F;
+
+        double d = 3.123454345343;
+
+/*### **Data Types **
+        Value they can store, Size(Range) -*/
+
+
+
+/*                1. Primitive Types
+        2. Non Primitive  Types , (Reference Type)
+
+#### **Primitive Types**
+        1. Boolean Type
+        2. Numeric Type
+        1. Intergral -> ( whole numbers) , no decimal)
+        1. byte
+        2. short
+        3. char
+        4. int
+        5. long
+        2. Floating ->. with decimal)
+        1. float
+        2. double
+
+#### **Reference or Non Primitive  Types**
+                - class, interface, enum, String*/
+
+
+
+
+
+
+
+
+    }
+}
