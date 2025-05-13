686012dc4d113f9526edaa7b3a719d56b085aeee
Krishna <knakum39@gmail.com>
2025-05-13 18:29:58 +0530

    13th May 2025 - Part 2

---
+++ src/ex_02_Java_Basics_Part2/Lab026_Local_Variable.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab026_Local_Variable {
+    public static void main(String[] args) {
+        int a =10;
+        byte b=10;
+        short s = 10;
+        long l = 9457845410L;
+        double d = 67.54564645465;
+        boolean b1 = true;
+        char c ='A';
+    }
+}
---
+++ src/ex_02_Java_Basics_Part2/Lab028_Final_Example.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab028_Final_Example {
+    public static void main(String[] args) {
+        int local;
+        local = 10;
+        System.out.println(local);
+
+     float PI = 3.14F;
+        // PI = 3.13f; This is final in nature.
+        System.out.println(PI);
+    }
+}
