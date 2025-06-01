a51caabb5b8487b646a27c99f48d7e14176a8dfa
Krishna Nakum <knakum39@gmail.com>
2025-06-01 18:45:19 +0530

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/ex_06_Ternary_Operator/Lab068_task3_17_05_Real_Age_Classification.java

--- src/Tasks/DaysInMonth.java
+++ src/Tasks/DaysInMonth.java
@@ -22,6 +22,7 @@ public class DaysInMonth {
         }
 
         switch (month) {
+            //case 1: case 3: case 5: case 7: case 8: case 10: case 12:
             case 1:  // January
             case 3:  // March
             case 5:  // May
@@ -31,6 +32,7 @@ public class DaysInMonth {
             case 12: // December
                 days = 31;
                 break;
+            //case 4: case 6: case 9: case 11:
             case 4:
             case 6:
             case 9:
