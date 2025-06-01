4a9c5c0cbdc0c4ab3d612a1039f5fa8e353c3ef2
Krishna Nakum <knakum39@gmail.com>
2025-06-01 19:22:24 +0530

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/ex_06_Ternary_Operator/Lab068_task3_17_05_Real_Age_Classification.java

---
+++ src/Tasks/Factorial_Of_n.java
@@ -0,0 +1,25 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Factorial_Of_n {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = scanner.nextInt();
+        int factorial =1;
+
+        if(number<0)
+        {
+            System.out.println("Factorial of -n is not possible");
+        }
+        else
+        {
+for(int i=1; i<=number;i++)
+{
+   factorial = factorial *i;
+}
+System.out.println("Factorial of "+ number + " is " +factorial);
+        }
+    }
+}
