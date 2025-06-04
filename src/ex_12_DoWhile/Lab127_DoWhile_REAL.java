a39d8b6c8d7144532551b1e68d7098eeb8f451f2
Krishna Nakum <knakum39@gmail.com>
2025-06-04 15:15:35 +0530

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/ex_06_Ternary_Operator/Lab068_task3_17_05_Real_Age_Classification.java

--- src/ex_12_DoWhile/Lab127_DoWhile_REAL.java
+++ src/ex_12_DoWhile/Lab127_DoWhile_REAL.java
@@ -6,10 +6,11 @@ public class Lab127_DoWhile_REAL {
         // Web Automation
         int number = 0;
         // 0 -> it can be 0 to 10 - elements or students
-        do{
+        do {
             System.out.println(number);
             // callFunction(number);
             // close the excel file, open the file.
             number++;
-        }while (number < 0);
-
+        } while (number < 0);
+    }
+}
