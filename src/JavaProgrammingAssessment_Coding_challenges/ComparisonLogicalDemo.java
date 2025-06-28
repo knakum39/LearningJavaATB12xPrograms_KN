018a3160e85eb5780d2ca0f7f63140b69d480bb7
Krishna Nakum <knakum39@gmail.com>
2025-06-28 23:18:00 +0530

    28th June

---
+++ src/JavaProgrammingAssessment_Coding_challenges/ComparisonLogicalDemo.java
@@ -0,0 +1,27 @@
+package JavaProgrammingAssessment_Coding_challenges;
+
+public class ComparisonLogicalDemo {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b = 20;
+
+        // Comparison Operators
+        System.out.println("Comparison Operators:");
+        System.out.println("a == b: " + (a == b));  // false
+        System.out.println("a != b: " + (a != b));  // true
+        System.out.println("a < b: " + (a < b));    // true
+        System.out.println("a > b: " + (a > b));    // false
+        System.out.println("a <= b: " + (a <= b));  // true
+        System.out.println("a >= b: " + (a >= b));  // false
+
+        // Logical Operators
+        boolean condition1 = (a<b);
+        boolean condition2 = (a>6);
+
+        System.out.println("\nLogical Oprators:");
+        System.out.println("Condition1 && condition2:" + (condition1 && condition2));
+        System.out.println("Condition1 || condition2:" + (condition1 || condition2));
+        System.out.println("!(Condition1):" + (!condition1));
+    }
+}
