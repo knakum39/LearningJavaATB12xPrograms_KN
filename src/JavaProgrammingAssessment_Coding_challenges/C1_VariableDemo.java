4c911a279ee53f7e83ed18077632f83ef6abfbb6
Krishna Nakum <knakum39@gmail.com>
2025-06-28 22:51:20 +0530

    28th June

---
+++ src/JavaProgrammingAssessment_Coding_challenges/C1_VariableDemo.java
@@ -0,0 +1,24 @@
+package JavaProgrammingAssessment_Coding_challenges;
+
+public class C1_VariableDemo {
+    public static void main(String[] args) {
+
+        // Integer variable
+        int studentAge = 15;
+
+        // Double variable
+        double averageScore = 88.75;
+
+        // Boolean variable
+        boolean isPassed = true;
+
+        // Character variable
+        char grade = 'A';
+
+        // Print all variable values
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Average Score: " + averageScore);
+        System.out.println("Passed: " + isPassed);
+        System.out.println("Grade: " + grade);
+    }
+}
